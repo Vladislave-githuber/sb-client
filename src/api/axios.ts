6f467cc46874000f9d0da2f6c8d2c96231d79cc2
Vladislav Gauss <vladireshetski@gmail.com>
2026-06-02 00:39:48 +0300

    first commit

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import toast from "react-hot-toast";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://api-sberbank-af152.up.railway.app",
   timeout: 10000,
   headers: { "Content-Type": "application/json" },
 });
